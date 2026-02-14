13827cd7f8c0fca9236db7c2c7982d4418954733
Kalana-indula <kalanasmedia@outlook.com>
2026-02-14 16:18:19 +0530

    login function added

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -6,9 +6,9 @@ const generateToken=(id)=>{
     return jwt.sign({id},process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXPIRE});
 };
 
-//@desc register user
+//@desc Register user
 //@route POST /api/v1/auth/signup
-//@access public
+//@access Public
 const signup=async (req,res)=>{
     try{
         const {name,email,password}=req.body;
@@ -58,6 +58,58 @@ const signup=async (req,res)=>{
         res.status(500).json({
             success:false,
             message:'Internal Server Error'
+        })
+    }
+}
+
+//@desc Login user
+//@route POST /api/v1/auth/login
+//@access Public
+const login=async (req,res)=>{
+    try {
+        const {email,password}=req.body;
+
+        //validation
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:'Please provide email and password'
+            })
+        }
+
+        //get the user by email an fetch the password
+        const user = await User.findOne({email}).select('password');
+
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:'Invalid credentials'
+            })
+        }
+
+        //check if passwords match
+        const isMatch = await user.matchPassword(password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                success:false,
+                message:'Invalid credentials'
+            });
+        }
+
+        res.status(200).json({
+            success:true,
+            data:{
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                token:generateToken(user._id)
+            }
+        })
+    }catch (error){
+        res.status(500).json({
+            success:false,
+            message:error.message
         })
     }
 }

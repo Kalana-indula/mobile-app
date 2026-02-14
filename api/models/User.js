85dd33f8ba4115cbbc474b9a412516803c3b74c3
Kalana-indula <kalanasmedia@outlook.com>
2026-02-14 13:34:29 +0530

    auth controller added

---
+++ api/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt=require('bcryptjs');
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please add name'],
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:[true,'Please enter a valid email'],
+        unique:true,
+        lowercase:true,
+        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please enter a valid email address'],
+    },
+    password:{
+        type:String,
+        required:[true,'Please enter your password'],
+        minLength:6
+    }
+},{timestamps:true});
+
+//encrypt password before saving...
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+});
+
+//match user entered password to hashed password in database
+userSchema.methods.matchPassword=async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
+module.exports=mongoose.model('User',userSchema);
